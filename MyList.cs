ffc231d2e6d4d14a5111692f23d112c6b83621d9
kliu154 <123418101+kliu154@users.noreply.github.com>
2023-02-03 09:36:42 -0800

    Add files via upload

---
+++ MyList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7
+{
+    public class MyList<T>
+    {
+        private List<T> list;
+
+        public MyList()
+        {
+            list = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        public T Remove(int index)
+        {
+            if (index < 0 || index >= list.Count)
+            {
+                throw new IndexOutOfRangeException("Index is out of range!");
+            }
+
+            T element = list[index];
+            list.RemoveAt(index);
+            return element;
+        }
+
+        public bool Contains(T element)
+        {
+            return list.Contains(element);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        public void InsertAt(T element, int index)
+        {
+            if (index < 0 || index >= list.Count)
+            {
+                throw new IndexOutOfRangeException("Index is out of range!");
+            }
+
+            list.Insert(index, element);
+        }
+
+        public void DeleteAt(int index) 
+        {
+            if (index < 0 || index >= list.Count)
+            {
+                throw new IndexOutOfRangeException("Index is out of range!");
+            }
+
+            list.RemoveAt(index);
+        }
+
+        public T Find(int index)
+        {
+            if (index < 0 || index >= list.Count)
+            {
+                throw new IndexOutOfRangeException("Index is out of range!");
+            }
+
+            return list[index];
+        }
+    }
+}
---
+++ MyStack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7
+{
+    public class MyStack<T>
+    {
+        private List<T> Stack;
+
+        public MyStack()
+        {
+            Stack = new List<T>();
+        }
+
+        public int Count()
+        {
+            return Stack.Count;
+        }
+
+        public T Pop()
+        {
+            if (Stack.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty!");
+            }
+
+            T item = Stack[Stack.Count - 1];
+            Stack.RemoveAt(Stack.Count - 1);
+
+            return item;
+        }
+
+        public void Push(T item)
+        {
+            Stack.Add(item);
+        }
+    }
+}
